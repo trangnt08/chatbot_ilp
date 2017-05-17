7b7fcf2b8bd146ec8f8120ab5350c7d045878b2f
trangnt08 <thuytrangnguyen1306@gmail.com>
2017-05-17 14:34:51 +0700

    hello world

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const WIT_TOKEN = process.env.WIT_TOKEN || '3YNNTFAQDIP76VLTFSKLMMNAZOGJWD7N'
+const WIT_TOKEN = process.env.WIT_TOKEN || 'OSJSQJ2VTJCKAQTC3DV3E7BLUGTWSMKS'
 if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAVMi6ykpokBAMmZBPsOHQi5k5wfbAGRtu7Kt8rprQJFN4LclZBhrT6tX5ZBUrXqzZBs3LLeQEq3gFPeyuy6E3cup8zrMXEMFBq24srQuQjvOMZAtKEB18SKIA4Sf0XxrQvBhYkuC2fhZCUzzgQBBkoI8Jo9qscefx0oFgOpSxqQZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAWVN5KkaeUBANWHg71us0173gZA0Xp4VHO7pTsdHl6XJeZAZBsozzFmE8KdLcZCOT1ZCkZCkVrAfF1CMlNoJZAerNKVGi2Qd1JPjzRgBZBsa0NLs5dLD2nLttSHW3GzuteZC0Ah1gFDRscZCf9w2aNNZA9qzL4aE8puIlbDkQ7oVVBVAZDZD';
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
