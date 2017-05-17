6fa8d9c86ba3dbc6985d584e2fce04ca872a3ccc
trangnt08 <thuytrangnguyen1306@gmail.com>
2017-05-17 14:59:10 +0700

    qlht

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (!WIT_TOKEN) {
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAWVN5KkaeUBANWHg71us0173gZA0Xp4VHO7pTsdHl6XJeZAZBsozzFmE8KdLcZCOT1ZCkZCkVrAfF1CMlNoJZAerNKVGi2Qd1JPjzRgBZBsa0NLs5dLD2nLttSHW3GzuteZC0Ah1gFDRscZCf9w2aNNZA9qzL4aE8puIlbDkQ7oVVBVAZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAEIatxxxLUBAC1bCUEm9BlPRveDbEGkvol8jaYoWZAZBripXCHSjNJyEc0DoI5dktda0ym2VZAW5qOIJQM5cIRBUGcraRo3LtTf5q5HEXoIbgG6FxtRYO0hf9D8R0tlyNZC2ah7DiaFuDiZCFfRNnVMxrFKzOGChyDgPh4DiiQZDZD';
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
